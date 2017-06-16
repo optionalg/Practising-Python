1372f22c577ed4659fc31496b6b1651d101e82fb
Babu <vikranth.info@gmail.com>
2017-06-16 16:23:29 +0530

    Add files via upload

---
+++ Basics/Day-8/OhYes2.py
@@ -0,0 +1,23 @@
+
+
+import os
+
+
+'''
+print(os.environ())
+
+print(os.getcwd())
+
+print(os.getgid())
+
+print(os.getuid())
+
+print(os.getpid())
+
+print(os.uname())
+
+print(help(os))
+
+print(dir(os))
+'''
+print(os.cpu_count())
