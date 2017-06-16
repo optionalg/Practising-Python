cb9a7a1d521b9584901575c88d1ee7ef75da08c5
Babu <vikranth.info@gmail.com>
2017-06-16 16:22:16 +0530

    Add files via upload

---
+++ Basics/Day-7/OhYes.py
@@ -0,0 +1,16 @@
+"""
+Check the os version wheter 32 or 64 bit mode.
+"""
+
+from os import *
+import os
+
+#print(dir(os))
+
+#help(os)
+
+import builtins
+
+print(dir(builtins))
+
+#print(os.DirEntry.__ospath__())
---
+++ Basics/Day-7/Os.py
@@ -0,0 +1,17 @@
+"""
+Program using operating system module.
+"""
+
+import os
+
+print(os.getcwd())
+
+dir(os) #list of module functions
+
+#help(os) #documentation
+
+#os.environ()
+print(os.getpid())
+
+#print(os.getuid())
+print(os.tmpnam())
